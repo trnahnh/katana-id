192b0fe42440e6fefd201121611fc18c95e387bf
khiem <khiemnguyen.hye@gmail.com>
2025-12-25 01:47:37 +0700

    Add temp register endpoint

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"log"
 	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"passwordHash"`
+}
+
+// Placeholder storage
+var users = make(map[string]string)
 
 func main() {
 	fmt.Print("Hello from the backend!")
@@ -14,6 +24,7 @@ func main() {
 
 	app := gin.Default()
 	app.GET("/health", healthCheck)
+	app.POST("/register", register)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -21,6 +32,24 @@ func main() {
 	}
 
 	app.Run(":" + port)
+}
+
+func register(c *gin.Context) {
+	var user User
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error generating password hash"})
+		return
+	}
+	users[user.Username] = string(hash)
+
+	c.JSON(200, gin.H{"message": "Registered successfully"})
 }
 
 func healthCheck(c *gin.Context) {
