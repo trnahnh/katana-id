b0af914df864cd36f82b957b3167ea018354d837
khiem <khiemnguyen.hye@gmail.com>
2025-12-27 00:55:29 +0700

    Add navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,20 +1,39 @@
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuTrigger } from "@radix-ui/react-navigation-menu"
-import { NavigationMenuList } from "./ui/navigation-menu"
+import { Button } from "./ui/button"
+import { ArrowRight } from 'lucide-react'
+import logo from '/logo.svg'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@radix-ui/react-hover-card"
+import { Card, CardContent } from "./ui/card"
 
 const NavBar = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="fixed top-0 w-full border-b border-border backdrop-blur-md">
-        <NavigationMenuItem className="flex items-center gap-6 px-6 py-6">
-          <div className="text-xl font-bold">KatanaID</div>
-          <NavigationMenuTrigger>Source</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Github</NavigationMenuLink>
-            <NavigationMenuLink>Website</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>)
+    <nav className="flex items-center justify-between w-full px-3 py-3 border-b-2 pl-10 pr-10">
+      <div className="flex items-center gap-2">
+        <img src={logo} className="h-8 hover:drop-shadow-[0_0_5px_rgba(59,130,246,0.8)] transition-shadow"></img>
+        <p className="font-mono font-bold hover:drop-shadow-[0_0_5px_rgba(59,130,246,0.8)] transition-shadow">Katana ID</p>
+      </div>
+      <div className="flex items-center gap-3">
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Button variant="ghost">Developers</Button>
+          </HoverCardTrigger>
+          <HoverCardContent className="flex gap-2">
+            <Card>
+              <CardContent className="p-2">
+                <Button variant="ghost">Khiem Nguyen</Button>
+                <Button variant="ghost">Anh Tran</Button>
+              </CardContent>
+            </Card>
+          </HoverCardContent>
+        </HoverCard>
+        <Button variant="ghost">Demo</Button>
+        <Button variant="ghost">Contact</Button>
+      </div>
+      <div className="flex items-center gap-3">
+        <Button variant="ghost" className="">Sign in</Button>
+        <Button variant="default" className="">Verify a media <ArrowRight /></Button>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/70",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -17,7 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/90",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
