314de5788ded0042cc78b70e70b8dc19df952b65
khiem <khiemnguyen.hye@gmail.com>
2026-01-10 18:57:42 +0700

    Remove duplicate handler

--- server/models/models.go
+++ server/models/models.go
@@ -19,11 +19,6 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
-type UsernameGenerationRequest struct {
-	Count string `json:"count"`
-	Vibe  string `json:"vibe"`
-}
-
 // Shared by both login and signup
 type AuthSuccessResponse struct {
 	Token         string `json:"token"`
@@ -32,23 +27,6 @@ type AuthSuccessResponse struct {
 	EmailVerified bool   `json:"email_verified"`
 }
 
-type VerificationSuccessResponse struct {
-	Message string `json:"message"`
-}
-
-type UsernameGenerationSuccessResponse struct {
-	Usernames string `json:"usernames"`
-}
-
 type ErrorResponse struct {
 	Error string `json:"error"`
-}
-
-type AvatarGenerationRequest struct {
-	Style  string `json:"style"`
-	Traits string `json:"traits"`
-}
-
-type AvatarGenerationSuccessResponse struct {
-	Image string `json:"image"`
 }
