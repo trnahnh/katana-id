767ec6d65f126e06565bd6bff4682a2e33bfb1ad
khiem <khiemnguyen.hye@gmail.com>
2026-02-18 23:32:04 +0700

    Split sender

--- server/auth/email.go
+++ server/auth/email.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 
 	"github.com/resend/resend-go/v2"
@@ -11,20 +12,22 @@ func sendVerificationEmail(token string, email string, username string) error {
 	client := resend.NewClient(os.Getenv("RESEND_API_KEY"))
 
 	link := fmt.Sprintf("%s/auth/verify-email?token=%s", os.Getenv("BACKEND_URL"), token)
+	names := []string{"Khiem", "Anh"}
+	from := names[rand.Intn(2)]
 
 	params := &resend.SendEmailRequest{
-		From:    "KatanaID <khiem@katanaid.com>",
+		From:    "KatanaID <verify@katanaid.com>",
 		To:      []string{email},
 		Subject: "KatanaID Email Verification",
 		Html: fmt.Sprintf(`
 		<p>Hello, %s</p>
 		<br>
-		<p>This is Khiem from KatanaID</p>
+		<p>This is %s from KatanaID</p>
 		<p>Welcome onboard</p>
 		<p>Click the link below to verify your email.</p>
 		<a href="%s">Verify Email</a>
 		<br>
-		<p>— Khiem from the The Katana ID Team</p>`, username, link),
+		<p>— %s From the The Katana ID Team</p>`, username, from, link, username),
 	}
 
 	_, err := client.Emails.Send(params)
