913680b556dfbc21ce44f98e87175104b312a74a
khiem <khiemnguyen.hye@gmail.com>
2026-03-19 21:34:57 +0700

    Remove unused imp

--- client/src/pages/public-pages/NotFoundPage.tsx
+++ client/src/pages/public-pages/NotFoundPage.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/button";
 const NotFoundPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4 text-center">
-      <h1 className="text-6xl font-bold">404</h1>
-      <p className="text-muted-foreground text-lg">Page not found</p>
+      <h1 className="text-8xl font-heading bg-linear-to-r from-violet-400 via-indigo-400 to-cyan-400 bg-clip-text text-transparent italic">404</h1>
+      <span className="text-lg">This is weird. <a className="text-muted-foreground text-lg hover:underline" href="">Report issue.</a></span>
       <Button asChild>
         <Link to="/">Go home</Link>
       </Button>
