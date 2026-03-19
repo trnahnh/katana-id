95a367659a383c696b8ccb320a153402aa3bb411
khiem <khiemnguyen.hye@gmail.com>
2026-03-19 21:14:29 +0700

    Minor changes

--- client/src/pages/public-pages/SignInPage.tsx
+++ client/src/pages/public-pages/SignInPage.tsx
@@ -1,8 +1,13 @@
+import logo from "/logo.svg";
 import { SignInForm } from "@/components/signin/SignInForm"
 
 const SignInPage = () => {
   return (
-    <div className="flex justify-center items-center mt-25 md:mt-50 mx-10 md:mx-0">
+    <div className="flex flex-col justify-center items-center mt-25 md:mt-50 mx-10 md:mx-0">
+      <div className="flex flex-col items-center gap-1 text-center mb-4">
+        <img src={logo} className="w-20" />
+        <h1 className="text-2xl font-bold pt-5">Sign in to KatanaID</h1>
+      </div>
       <SignInForm />
     </div>
   )
